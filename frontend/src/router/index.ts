dff3d149845a6eee11076c8bd1e1d11544cf6e80
isuri54 <154318794+isuri54@users.noreply.github.com>
2026-06-18 23:23:11 +0530

    Update frontend routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,12 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import App from '../App.vue'; 
 import SignupView from '../views/common/Signup/SignupView.vue';
+import LoginView from '../views/common/Login/LoginView.vue';
+import CustomerDashboard from '../views/customer/dashboard/CustomerDashboard.vue';
+import CategoriesView from '../views/customer/categories/CategoriesView.vue';
 
 const routes = [
   {
     path: '/',
     name: 'signup',
     component: SignupView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/cusdashboard',
+    name: 'customer-dashboard',
+    component: CustomerDashboard
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    component: CategoriesView
   }
   // Future roles routes (e.g., /renter, /customer) 
 ];
