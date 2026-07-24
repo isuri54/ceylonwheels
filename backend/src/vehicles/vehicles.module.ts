cd212448469a6fc00d439cb7f841bd26eb3cd012
isuri54 <154318794+isuri54@users.noreply.github.com>
2026-07-25 00:08:53 +0530

    Update vehicles.module.ts

--- backend/src/vehicles/vehicles.module.ts
+++ backend/src/vehicles/vehicles.module.ts
@@ -3,10 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { VehiclesController } from './vehicles.controller';
 import { VehiclesService } from './vehicles.service';
 import { Vehicle, VehicleSchema } from './schemas/vehicle.schema';
+import { SavedVehicle, SavedVehicleSchema } from './schemas/saved-vehicle.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }]),
+    MongooseModule.forFeature([
+      { name: Vehicle.name, schema: VehicleSchema },
+      { name: SavedVehicle.name, schema: SavedVehicleSchema }
+    ]),
   ],
   controllers: [VehiclesController],
   providers: [VehiclesService],
